9407f4bf93aeca935b8bdb3cb22cadb8619813a5
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2010-04-10 21:21:38 +0200

    Some more changes.

--- puget_sound/download.sh
+++ puget_sound/download.sh
@@ -1,2 +1,8 @@
-wget http://www-static.cc.gatech.edu/data_files/large_models/ps/ps_height_16k.png
-wget http://www-static.cc.gatech.edu/data_files/large_models/ps/ps_texture_16k.png
+[ -f ps_height_1k.png ] || wget http://www-static.cc.gatech.edu/data_files/large_models/ps/ps_height_1k.png
+[ -f ps_texture_1k.png ] || wget http://www-static.cc.gatech.edu/data_files/large_models/ps/ps_texture_1k.png
+
+[ -f ps_height_4k.png ] || wget http://www-static.cc.gatech.edu/data_files/large_models/ps/ps_height_4k.png
+[ -f ps_texture_4k.png ] || wget http://www-static.cc.gatech.edu/data_files/large_models/ps/ps_texture_4k.png
+
+[ -f ps_height_16k.png ] || wget http://www-static.cc.gatech.edu/data_files/large_models/ps/ps_height_16k.png
+[ -f ps_texture_16k.png ] || wget http://www-static.cc.gatech.edu/data_files/large_models/ps/ps_texture_16k.png
--- puget_sound/generate.sh
+++ puget_sound/generate.sh
@@ -1,1 +1,8 @@
-osgdem --xx 10 --yy 10 -t ps_texture_16k.png --xx 10 --yy 10 -d ps_height_16k.png -l 8 -v 0.1 -o puget.ive -a pegout.osga
+gdal_translate ps_height_16k.png ps_height_16k.tif
+gdaladdo -r average ps_height_16k.tif 2 4 8 16 32
+
+gdal_translate ps_texture_16k.png ps_texture_16k.tif
+gdaladdo -r average ps_texture_16k.tif 2 4 8 16 32
+
+osgdem --xx 10 --yy 10 -t ps_texture_16k.tif --xx 10 --yy 10 -d ps_height_16k.tif -l 2 -v 0.1 -o puget.ive -a pegout.osga
+#osgdem --xx 10 --yy 10 -t ps_texture_16k.tif --xx 10 --yy 10 -d ps_height_16k.tif -l 2 -v 0.1 -o puget.ive -a pegout.osga
